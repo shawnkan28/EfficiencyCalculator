11a95bfe56fd88919d36fc05bd9c67e6915b4ced
SK <shawn.work.script@gmail.com>
2022-10-22 22:47:02 +0800

    print more meaningful  error msg

--- artery_gear.py
+++ artery_gear.py
@@ -17,14 +17,14 @@ log = Logger(log_path).get_logger()
 
 def main():
     if not args.gear_stat.is_file():
-        log.error("Unable to locate csv file of gear stats.")
+        log.error(f"Unable to locate csv file of gear stats. {args.gear_stat}")
         gear_df = pd.DataFrame()
     else:
         log.info(f"Reading Gear Stats from {args.gear_stat}")
         gear_df = pd.read_csv(args.gear_stat, index_col=0)
 
     if not args.db.is_file():
-        log.error("Unable to locate csv file of gear stats.")
+        log.error(f"Unable to locate csv file of gear stats. {args.db}")
         db_df = pd.DataFrame()
     else:
         log.info(f"Reading Gear Stats from {args.db}")
