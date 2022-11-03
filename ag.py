94ea29d1360a063c67936a905da6934278402c10
SK <shawn.work.script@gmail.com>
2022-11-03 20:52:23 +0800

    clean everything start from scratch

--- ag.py
+++ ag.py
@@ -1,11 +1,6 @@
-from PyQt5 import QtWidgets
-
 import helper as h
 from Logger import Logger
 from Parameter import Parameters
-from crawler import Crawler
-from ui_window import Window
-from getter_setter import GS
 
 """ Initializing Script Parameters """
 args = Parameters(description="Framework project").parse()
@@ -17,31 +12,7 @@ h.clean_logs(log_path.parent, log=log)
 
 
 def main():
-    f_path, thumb_path = args.var_dir / "character_db.pickle", args.var_dir / "thumb"
-    main_path, sub_path = args.var_dir / "gear_main.pickle", args.var_dir / "gear_sub.pickle"
-    crawl = Crawler(args.base_url, log)
-    # pull and save character information from Server in a file
-    try:
-        crawl.all_characters(f_path, thumb_path)
-        crawl.gear_stats(main_path, sub_path)
-    except Exception as e:
-        log.debug(e)
-        log.warning(f"Failed to pull from website. Using Local File ...")
-
-    gs = GS(thumb_path, f_path, main_path, sub_path, log)
-
-    app(gs)
-
-
-def app(gs):
-    q_app = QtWidgets.QApplication([])
-
-    # set GUI
-    window = Window(gs)
-    window.show()
-
-    # Run the App
-    q_app.exec_()
+    log.info("test")
 
 
 if __name__ == '__main__':
