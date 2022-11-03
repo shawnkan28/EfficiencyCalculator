ac330e55b1eafdcf034d583f6cef10269d19b6e5
SK <shawn.work.script@gmail.com>
2022-11-03 19:25:38 +0800

    stuff for computation in window.py

--- ui_window.py
+++ ui_window.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets as qtw, QtGui as qtg
 from ui_character_info import CharInfo
 from ui_efficiency_info import EffInfo
+import pandas as pd
 
 
 class Window(qtw.QWidget):
@@ -9,8 +10,22 @@ class Window(qtw.QWidget):
         self.gs = gs
         self.btn = None
 
+        # UI Design
         self._init_ui()
 
+        # Dataframes
+        self.gear_main_df = gs.gm_df
+
+        # Widget Variables
+        self.eff_score_lbl = gs.eff_score_widgets
+        self.var_gear_widgets = gs.var_gear_widgets
+
+        # Stats
+        self.main_gear_stats = pd.Series(dtype=float)
+
+        self._compute_main_gear_stats()
+
+        # Show UI interface
         self._set_location()
         self.show()
 
@@ -36,3 +51,25 @@ class Window(qtw.QWidget):
         btn = qtw.QPushButton("Save")
         self.btn = btn
         return btn
+
+    def _compute_main_gear_stats(self):
+        data = []
+        var_gears = [x.replace("_cb", "") for x in self.var_gear_widgets.index]
+
+        for gear in self.gear_main_df.columns:
+            stat = self.gear_main_df.loc[self.gear_main_df[gear].notnull(), gear]
+
+            if gear in var_gears:  # variable stat gears
+                stat_type = self.var_gear_widgets.loc[f"{gear}_cb"].currentText()
+                stat_val = stat.loc[stat_type]
+            else:  # static stat gears (Gun/Core/Plate)
+                stat_type, stat_val = stat.index[0], stat.iloc[0]
+            # create dataframe data
+            data.append({gear: stat_val, "index": stat_type})
+
+        # self.main_gear_stats = pd.concat([self.main_gear_stats, pd.Series()])
+        # print(self.var_gear_widgets)
+        # print(self.gear_main_df)
+        df = pd.DataFrame(data)
+        df.set_index('index', inplace=True)
+        print(df)
